82f4bd17bc4bcfbc2b42e22420d5390ab7215664
odeoodi <odeo@metropolia.fi>
2025-08-25 10:36:49 +0300

    React activity 2 part 2

---
+++ week2-fe-lab6/src/App.jsx
@@ -0,0 +1,13 @@
+import Greetings from './Greetings'
+import './App.css'
+
+function App() {
+  return (
+    <div classname='App'>
+        <Greetings name='Alice' message='Welcome to the lab!'/>
+        <Greetings name='Bob' message='Good morning!'/>
+    </div>
+  );
+}
+
+export default App
---
+++ week2-fe-lab6/src/Greetings.jsx
@@ -0,0 +1,12 @@
+function Greetings(props) {
+    console.log(props);
+
+    return (
+        <div className="greeting">
+            <h1>Welcome, {props.name}!</h1>
+            <p>{props.message}</p>
+        </div>
+    );
+}
+
+export default Greetings;
