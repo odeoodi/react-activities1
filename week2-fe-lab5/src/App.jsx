ee012196814060ff4870f6e9f46a864df7d665dc
odeoodi <odeo@metropolia.fi>
2025-08-25 10:28:10 +0300

    React activity 2 part 1

---
+++ week2-fe-lab5/src/App.jsx
@@ -0,0 +1,16 @@
+import Footer from './Footer'
+import Header from './Header'
+import MainContent from './MainContent'
+import './App.css'
+
+function App() {
+  return(
+    <div>
+      <Header/>
+      <MainContent/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
